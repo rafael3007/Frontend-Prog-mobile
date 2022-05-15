0c1800ef025470824ec2a1b09f6366ae2fd23964
rafael3007 <rafael.brito.1422@gmail.com>
2022-05-14 23:43:17 -0300

    menu

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,20 +1,20 @@
 
 import { Text,View,TouchableOpacity, StyleSheet } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Texto } from "./HomeCss";
+import { Texto, Container, Botao } from "./HomeCss";
 
 
 export default function Home() {
     return(
         <SafeAreaView >
-            <View>
-                <TouchableOpacity>
+            <Container>
+                <Botao>
                     <Texto styles={{color:'red'}}>Motoristas</Texto>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>Veiculos</Text>
-                </TouchableOpacity>
-            </View>
+                </Botao>
+                <Botao>
+                    <Texto>Veiculos</Texto>
+                </Botao>
+            </Container>
         </SafeAreaView>
     )
 };
